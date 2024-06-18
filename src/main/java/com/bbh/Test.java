5a5a04342d64950198b5a36ec2758e964b35de12
bbh <bbh@163.com>
2024-06-18 22:46:14 +0800

    div branch commit

--- src/main/java/com/bbh/Test.java
+++ src/main/java/com/bbh/Test.java
@@ -11,5 +11,6 @@ package com.bbh;
 public class Test {
     public static void main(String[] args) {
             System.out.println("Hello");
-        }
+        System.out.println("hello git");
+    }
     }
